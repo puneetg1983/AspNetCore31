82a175d60f74e7737a15316ebc58f648b92e6a53
Puneet Gupta <puneetg@microsoft.com>
2021-08-23 16:59:53 +0530

    Changing to restart every 1 hour

---
+++ AspNetCore31/RestartSiteService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCore31
+{
+    internal class RestartSiteService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
+                Environment.FailFast("Restarting the process");
+            }
+        }
+    }
+}
--- AspNetCore31/Startup.cs
+++ AspNetCore31/Startup.cs
@@ -24,6 +24,7 @@ namespace AspNetCore31
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddHostedService<RestartSiteService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
